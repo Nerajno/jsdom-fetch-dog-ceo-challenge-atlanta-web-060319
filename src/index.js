fef759056c67e774a1f364dbfbc1438e6d8078fc
Nerajno <Nerajno@gmail.com>
2019-07-20 21:49:37 -0400

    Some progress made

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener("DOMContentLoaded", challengeOne)
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function createDog(dog) {
+    const dogList = document.getElementById("dog-image-container")
+    
+    let section = document.createElement("section")
+    let img = document.createElement("img")
+    img.src = dog
+    section.appendChild(img)
+    dogList.appendChild(section)
+    console.log(dog)
+}
+
+
+function createDoglist(data) {
+    data.message.forEach(createDog)
+}
+
+function challengeOne(){
+    fetch(imgUrl)
+    .then(response => response.json() )
+    .then(data => createDoglist(data))
+ }
+
